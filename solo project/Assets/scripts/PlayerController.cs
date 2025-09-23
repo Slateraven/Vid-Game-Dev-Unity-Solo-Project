98539786b222c377f93d3641d3ddd50f6e363f8f
Slateraven <dinhand529@gmail.com>
2025-09-23 13:39:11 -0700

    ladders....suck an egg...also u weapons

--- solo project/Assets/scripts/PlayerController.cs
+++ solo project/Assets/scripts/PlayerController.cs
@@ -12,6 +12,14 @@ public class PlayerController : MonoBehaviour
 
     Ray jumpRay;
 
+    Ray interactRay;
+    RaycastHit interactHit;
+    GameObject pickupObj;
+
+    public PlayerInput input;
+    public Transform weaponSlot;
+    public Weapon currentWeapon; 
+
     float inputX;
     float inputY;
     
@@ -23,18 +31,24 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 5f;
     public float jumpRayDistance = 1.1f;
     public float calculationLimit = 90;
-    public float ladderForce;
+    public float interactDistance = 1f; 
 
     public int health = 5;
     public int maxHealth = 5;
 
+    public bool attacking = false;
+    public bool climbing = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        input = GetComponent<PlayerInput>();
+        interactRay = new Ray(transform.position, transform.forward);
         jumpRay = new Ray(transform.position, -transform.up);
         rb = GetComponent<Rigidbody>();
         playerCam = Camera.main;
         lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+        weaponSlot = playerCam.transform.GetChild(0); 
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -65,14 +79,80 @@ public class PlayerController : MonoBehaviour
         jumpRay.origin = transform.position;
         jumpRay.direction = -transform.up;
 
+        interactRay.origin = playerCam.transform.position;
+        interactRay.direction = playerCam.transform.forward;
+
+        if (Physics.Raycast(interactRay, out interactHit, interactDistance))
+        {
+            if (interactHit.collider.tag == "weapon")
+            {
+                pickupObj = interactHit.collider.gameObject;
+            }
+        }
+        else
+            pickupObj = null;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && attacking)
+                currentWeapon.fire();
+
+       
+
         //Movement System
 
         Vector3 tempMove = rb.linearVelocity;
 
-        tempMove.x = inputY * speed;
+        if (climbing)
+            tempMove.y = inputY * speed;
+        else
+            tempMove.x = inputY * speed;
+
         tempMove.z = inputX * speed;
 
-        rb.linearVelocity = (tempMove.x * transform.forward) + (tempMove.y * ladderForce * transform.up) + (tempMove.z * transform.right);
+        rb.linearVelocity = (tempMove.x * transform.forward) + (tempMove.y * transform.up) + (tempMove.z * transform.right);
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (currentWeapon)
+        {
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    attacking = true;
+                else
+                    attacking = false;
+            }
+
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire(); 
+        }
+    }
+
+    public void Reload()
+    {
+        if (currentWeapon)
+            currentWeapon.reload();
+    }
+
+    public void Interact()
+    {
+        if (pickupObj)
+        {
+            if (pickupObj.tag == "weapon")
+                pickupObj.GetComponent<Weapon>().equip(this);
+
+        }
+        else
+            Reload();
+    }
+
+    public void DropWeapon()
+    {
+        if (currentWeapon)
+        {
+            currentWeapon.GetComponent<Weapon>().unequip(); 
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -117,24 +197,18 @@ public class PlayerController : MonoBehaviour
         {
             health--;
         }
-
         if (collision.gameObject.tag == "ladder")
         {
-            print("ladder hit");
-            ladderForce = 2f;
-
+            climbing = true;
         }
 
     }
     private void OnCollisionExit(Collision collision)
     {
+
         if (collision.gameObject.tag == "ladder")
         {
-            ladderForce = 1f;
-
+            climbing = false;
         }
     }
-       
- 
-
 }
